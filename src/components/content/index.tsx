b4537e31cdfac8c3163bb4854411a0d0fba4322b
knightndgale <knightndgale@gmail.com>
2023-04-15 10:42:44 +0800

    :bookmark: Optimize main page

---
+++ src/components/content/index.tsx
@@ -0,0 +1,109 @@
+import { useSession } from "next-auth/react";
+import { type RouterOutputs, api } from "../../utils/api";
+import { useState } from "react";
+import NoteEditor from "../noteEditor";
+import NoteCard from "../noteCard";
+type Topic = RouterOutputs["topic"]["getAll"][0];
+
+const Content: React.FC = () => {
+  const { data: sessionData } = useSession();
+
+  const [selectedTopic, setSelectedTopic] = useState<Topic | null>(null);
+
+  const { data: topics, refetch: refetchTopics } = api.topic.getAll.useQuery(
+    undefined, // no input
+    {
+      enabled: sessionData?.user !== undefined,
+      onSuccess: (data) => {
+        setSelectedTopic(selectedTopic ?? data[0] ?? null);
+      },
+    }
+  );
+
+  const createTopic = api.topic.create.useMutation({
+    onSuccess: () => {
+      void refetchTopics();
+    },
+  });
+
+  const { data: notes, refetch: refetchNotes } = api.note.getAll.useQuery(
+    {
+      topicId: selectedTopic?.id ?? "",
+    },
+    {
+      enabled: sessionData?.user !== undefined && selectedTopic !== null,
+    }
+  );
+
+  const createNote = api.note.create.useMutation({
+    onSuccess: () => {
+      void refetchNotes();
+    },
+  });
+
+  const deleteNote = api.note.delete.useMutation({
+    onSuccess: () => {
+      void refetchNotes();
+    },
+  });
+
+  return (
+    <div className="mx-5 mt-5 grid grid-cols-4 gap-2">
+      <div className="px-2">
+        <ul className="w-100 menu rounded-box bg-base-100 p-2">
+          {topics?.map((topic) => (
+            <li key={topic.id}>
+              <a
+                href="#"
+                onClick={(evt) => {
+                  evt.preventDefault();
+                  setSelectedTopic(topic);
+                }}
+              >
+                {topic.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="divider"></div>
+        <input
+          type="text"
+          placeholder="New Topic"
+          className="input-bordered input input-sm w-full"
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              createTopic.mutate({
+                title: e.currentTarget.value,
+              });
+              e.currentTarget.value = "";
+            }
+          }}
+        />
+      </div>
+      <div className="col-span-3">
+        <div>
+          {notes?.map((note) => (
+            <div key={note.id} className="mt-5">
+              <NoteCard
+                note={note}
+                onDelete={() => void deleteNote.mutate({ id: note.id })}
+              />
+            </div>
+          ))}
+        </div>
+
+        <NoteEditor
+          onSave={({ title, content }) => {
+            void createNote.mutate({
+              title,
+              content,
+              topicId: selectedTopic?.id ?? "",
+            });
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Content;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,8 @@
-import { useState } from "react";
 import { type NextPage } from "next";
 import Head from "next/head";
-import { useSession } from "next-auth/react";
 
-import { api, type RouterOutputs } from "../utils/api";
-import NoteCard from "~/components/noteCard";
 import Header from "~/components/header";
-import NoteEditor from "~/components/noteEditor";
+import Content from "~/components/content";
 
 const Home: NextPage = () => {
   return (
@@ -25,106 +21,3 @@ const Home: NextPage = () => {
 };
 
 export default Home;
-
-type Topic = RouterOutputs["topic"]["getAll"][0];
-
-const Content: React.FC = () => {
-  const { data: sessionData } = useSession();
-
-  const [selectedTopic, setSelectedTopic] = useState<Topic | null>(null);
-
-  const { data: topics, refetch: refetchTopics } = api.topic.getAll.useQuery(
-    undefined, // no input
-    {
-      enabled: sessionData?.user !== undefined,
-      onSuccess: (data) => {
-        setSelectedTopic(selectedTopic ?? data[0] ?? null);
-      },
-    }
-  );
-
-  const createTopic = api.topic.create.useMutation({
-    onSuccess: () => {
-      void refetchTopics();
-    },
-  });
-
-  const { data: notes, refetch: refetchNotes } = api.note.getAll.useQuery(
-    {
-      topicId: selectedTopic?.id ?? "",
-    },
-    {
-      enabled: sessionData?.user !== undefined && selectedTopic !== null,
-    }
-  );
-
-  const createNote = api.note.create.useMutation({
-    onSuccess: () => {
-      void refetchNotes();
-    },
-  });
-
-  const deleteNote = api.note.delete.useMutation({
-    onSuccess: () => {
-      void refetchNotes();
-    },
-  });
-
-  return (
-    <div className="mx-5 mt-5 grid grid-cols-4 gap-2">
-      <div className="px-2">
-        <ul className="w-100 menu rounded-box bg-base-100 p-2">
-          {topics?.map((topic) => (
-            <li key={topic.id}>
-              <a
-                href="#"
-                onClick={(evt) => {
-                  evt.preventDefault();
-                  setSelectedTopic(topic);
-                }}
-              >
-                {topic.title}
-              </a>
-            </li>
-          ))}
-        </ul>
-        <div className="divider"></div>
-        <input
-          type="text"
-          placeholder="New Topic"
-          className="input-bordered input input-sm w-full"
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              createTopic.mutate({
-                title: e.currentTarget.value,
-              });
-              e.currentTarget.value = "";
-            }
-          }}
-        />
-      </div>
-      <div className="col-span-3">
-        <div>
-          {notes?.map((note) => (
-            <div key={note.id} className="mt-5">
-              <NoteCard
-                note={note}
-                onDelete={() => void deleteNote.mutate({ id: note.id })}
-              />
-            </div>
-          ))}
-        </div>
-
-        <NoteEditor
-          onSave={({ title, content }) => {
-            void createNote.mutate({
-              title,
-              content,
-              topicId: selectedTopic?.id ?? "",
-            });
-          }}
-        />
-      </div>
-    </div>
-  );
-};
