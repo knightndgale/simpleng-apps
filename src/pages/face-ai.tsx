837eb38c9b2be3a6c1d8247501e47ee691a2a8b5
knightndgale <knightndgale@gmail.com>
2023-04-16 22:26:08 +0800

    :adhesive_bandage: change image dismissal properly

--- src/pages/face-ai.tsx
+++ src/pages/face-ai.tsx
@@ -13,15 +13,18 @@ const FaceAI: React.FC = () => {
   };
 
   const handleImageSelect = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    const reader = new FileReader();
-
-    if (!file) {
-      dismissImage;
-      return;
+    if (!event.target.files || event.target.files?.length < 1) {
+      return dismissImage();
     }
 
+    const file = event.target.files?.[0];
+    if (!file) return;
+    const reader = new FileReader();
+
     reader.onload = (_event) => {
+      const arrayBuffer = reader.result as ArrayBuffer;
+      const buffer = Buffer.from(arrayBuffer);
+      console.log(buffer);
       setImageUrl(_event.target?.result as string);
     };
 
@@ -29,15 +32,15 @@ const FaceAI: React.FC = () => {
   };
 
   return (
-    <div className="grid max-h-screen grid-cols-4 gap-2">
-      <div className="col-span-1 max-h-screen pt-5">
+    <div className="grid min-h-fit grid-cols-4 gap-2">
+      <div className="col-span-1  pt-5">
         <div className="relative">
           <input
             ref={fileInputRef}
             type="file"
             onChange={handleImageSelect}
             accept=".jpeg, .png, .jpg"
-            className="w-full cursor-pointer truncate rounded-md border border-gray-300 py-2 pl-10 pr-4 text-sm text-gray-500 file:hidden focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500 sm:text-sm"
+            className="w-full cursor-pointer truncate rounded-md border border-gray-300 py-2 pl-10 pr-14 text-sm text-gray-500 file:hidden focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500 sm:text-sm"
           />
           <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
             <PhotoIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
@@ -47,7 +50,7 @@ const FaceAI: React.FC = () => {
         <div className="divider"></div>
       </div>
 
-      <div className="col-span-3 flex max-h-screen flex-wrap  gap-3 p-5  ">
+      <div className="col-span-3 flex flex-wrap  gap-3 p-5  ">
         <div className="relative">
           {/* <div className="absolute right-20 top-20  box-border h-32 w-32 border-4 border-blue-700 p-4 "></div> */}
 
@@ -59,7 +62,7 @@ const FaceAI: React.FC = () => {
           />
           {imageUrl && fileInputRef.current?.value && (
             <button
-              className="absolute right-0 top-0 m-5 rounded-full p-1 hover:bg-primary hover:text-white focus:bg-primary focus:outline-none"
+              className="absolute right-0 top-0 m-5 rounded-full bg-primary p-1 text-white transition ease-in-out hover:-translate-y-1  hover:scale-105"
               onClick={() => dismissImage()}
             >
               <XMarkIcon className="h-6 w-6 " />
