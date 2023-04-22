3622bd613da0ea8fd7acf3f3af3f82795082b109
knightndgale <knightndgale@gmail.com>
2023-04-22 16:19:22 +0800

    :wrench: add meta data

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, NextScript, Head, Main } from "next/document";
 export default function Document() {
   return (
     <Html data-theme="corporate">
-      <Head />
+      <Head>
+        <title>Simpleng Apps</title>
+        <meta name="author" content="Mark Dave Soriano" />
+        <meta
+          name="description"
+          content="Discover Next.js, Prisma, PostgreSQL, useQuery, and Redux-powered web apps, curated by Mark Dave Soriano and the Innoendo IT Solutions team. Join us on this exciting web adventure!"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
